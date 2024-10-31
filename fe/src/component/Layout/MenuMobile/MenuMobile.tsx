22b569e31725fb197c03a5011a7483bf7f0c9fd2
ngochuyk812 <ngohuyk80169@gmail.com>
2024-10-31 18:05:11 +0700

    Refactor mobile menu component and styles

--- fe/src/component/Layout/MenuMobile/MenuMobile.tsx
+++ fe/src/component/Layout/MenuMobile/MenuMobile.tsx
@@ -1,37 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import './style.css'
 import { useNavigate } from 'react-router-dom';
-const MenuMobile : React.FC = () =>{
+const MenuMobile: React.FC = () => {
     const [active, setActive] = useState("")
     const navigator = useNavigate()
-    const handleMenu = ( path: string)=>{
+    const handleMenu = (path: string) => {
         setActive(path)
         navigator(path);
     }
     console.log(location.pathname);
-    
-    const menu =[
-        {path:"/", name: "Home", icon:"fa-solid fa-house"},
-        {path:"/friends", name: "Friends", icon:'fa-solid fa-user-group'},
-        {path:"/messages", name: "Messages", icon:'fa-solid fa-message'},
-        {path:"/notify", name: "Notification", icon : 'fa-solid fa-bell'},
-        {path:"/profile", name: "Profile", icon:'fa-solid fa-user'},
+
+    const menu = [
+        { path: "/", name: "Home", icon: "fa-solid fa-house" },
+        { path: "/friends", name: "Friends", icon: 'fa-solid fa-user-group' },
+        { path: "/messages", name: "Messages", icon: 'fa-solid fa-message' },
+        { path: "/notify", name: "Notification", icon: 'fa-solid fa-bell' },
+        { path: "/profile", name: "Profile", icon: 'fa-solid fa-user' },
 
 
     ]
-    useEffect(()=>{
+    useEffect(() => {
         setActive(location.pathname)
-    },[location.pathname])
+    }, [location.pathname])
     return (
-       <div className='mobile-menu'>
-        <ul>
-                {menu.map((tmp,index)=>{
-                    return <li key={index} onClick={()=>{handleMenu(tmp.path)}} className={active == tmp.path ? 'active' : "not-active"}><i className={tmp.icon}></i> <p style={{fontSize:'13px'}}>{tmp.name}</p></li>
+        <div className='height-mobile-menu'>
+            <div className='mobile-menu height-mobile-menu'>
+                <ul>
+                    {menu.map((tmp, index) => {
+                        return <li key={index} onClick={() => { handleMenu(tmp.path) }} className={active == tmp.path ? 'active' : "not-active"}><i className={tmp.icon}></i> <p style={{ fontSize: '13px' }}>{tmp.name}</p></li>
 
-                })}
-              
-            </ul>
-       </div>
+                    })}
+
+                </ul>
+            </div>
+        </div>
     );
 }
 
