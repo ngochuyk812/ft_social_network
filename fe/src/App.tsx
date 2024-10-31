07cb01dd669844da16aefe402113f2ff4bab2f07
ngochuyk812 <ngohuyk80169@gmail.com>
2024-10-31 17:49:50 +0700

    update env

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -11,88 +11,90 @@ import { addNotify } from './redux/slice/notifySlice'
 import { Message } from './types/message'
 import { addMessToRoom } from './redux/slice/messageSlice'
 import Loading from './component/Loading/Loading'
+const baseUrl = import.meta.env.VITE_URL_BE;
 
 function App() {
   const [connection, setConnection] = useState<null | HubConnection>(null);
-  const token = useSelector((state:RootState)=>state.auth.user?.accessToken)
-  const id = useSelector((state:RootState)=>state.auth.user?.id)
+  const token = useSelector((state: RootState) => state.auth.user?.accessToken)
+  const id = useSelector((state: RootState) => state.auth.user?.id)
 
   const dispatch = useDispatch();
   useEffect(() => {
-      
+
     const connect = new HubConnectionBuilder()
-      .withUrl("https://localhost:7210/hubs/notification",{
+      .withUrl(baseUrl + "hubs/notification", {
         transport: HttpTransportType.WebSockets,
         accessTokenFactory: () => {
-            return `${token}`
-        }})
+          return `${token}`
+        }
+      })
       .withAutomaticReconnect()
       .build();
-        
+
     setConnection(connect);
-    
+
   }, [token]);
 
-  
+
   useEffect(() => {
     if (connection) {
 
       connection
         .start()
         .then(() => {
-          
-          connection.on("Receive", ( res : SignalRRes) => {
-            
-            if(res.type == 1){
-              const dataRes =  res.data
+
+          connection.on("Receive", (res: SignalRRes) => {
+
+            if (res.type == 1) {
+              const dataRes = res.data
               const n = dataRes as Notifi
 
-              if(n.type === 0){
-                dispatch(addNotify({message:"Bài viết", description: n.from.fullName+" đã thích bài viết của bạn", type:'success'}))
+              if (n.type === 0) {
+                dispatch(addNotify({ message: "Bài viết", description: n.from.fullName + " đã thích bài viết của bạn", type: 'success' }))
               }
-              if(n.type === 1){
-                dispatch(addNotify({message:"Bài viết", description: n.from.fullName+" đã bình luận bài viết của bạn", type:'success'}))
+              if (n.type === 1) {
+                dispatch(addNotify({ message: "Bài viết", description: n.from.fullName + " đã bình luận bài viết của bạn", type: 'success' }))
               }
-              if(n.type === 2){
-                  dispatch(addNotify({message:"Bạn bè", description: n.from.fullName+" đã gửi lời mời kết bạn đến bạn", type:'success'}))
+              if (n.type === 2) {
+                dispatch(addNotify({ message: "Bạn bè", description: n.from.fullName + " đã gửi lời mời kết bạn đến bạn", type: 'success' }))
               }
-              if(n.type === 3){
-                dispatch(addNotify({message:"Bạn bè", description: n.from.fullName+" đã chấp nhận lời mời kết bạn đến bạn", type:'success'}))
+              if (n.type === 3) {
+                dispatch(addNotify({ message: "Bạn bè", description: n.from.fullName + " đã chấp nhận lời mời kết bạn đến bạn", type: 'success' }))
               }
-              if(n.type === 4){
-                dispatch(addNotify({message:"Bình luận", description: n.from.fullName+" đã trả lời bình luận đến bạn", type:'success'}))
+              if (n.type === 4) {
+                dispatch(addNotify({ message: "Bình luận", description: n.from.fullName + " đã trả lời bình luận đến bạn", type: 'success' }))
               }
-              if(n.type === 5){
-                dispatch(addNotify({message:"Tin nhắn", description: n.from.fullName+" đã gửi cho bạn một tin nhắn", type:'success'}))
+              if (n.type === 5) {
+                dispatch(addNotify({ message: "Tin nhắn", description: n.from.fullName + " đã gửi cho bạn một tin nhắn", type: 'success' }))
               }
             }
 
-            if(res.type === 0){
-              const dataRes =  res.data
+            if (res.type === 0) {
+              const dataRes = res.data
               const m = dataRes as Message
-              dispatch(addMessToRoom({data: [m], location: 1,roomId: m.roomId}))
-              if(m.userId !== id )
-              dispatch(addNotify({message:"Tin nhắn", description: m.user.fullName+" đã gửi cho bạn một tin nhắn", type:'success'}))
+              dispatch(addMessToRoom({ data: [m], location: 1, roomId: m.roomId }))
+              if (m.userId !== id)
+                dispatch(addNotify({ message: "Tin nhắn", description: m.user.fullName + " đã gửi cho bạn một tin nhắn", type: 'success' }))
 
             }
-            
+
           });
         })
         .catch((error) => console.log(error));
     }
 
-  }, [connection]); 
-  const isLoading = useSelector((state:RootState) => state.auth);
+  }, [connection]);
+  const isLoading = useSelector((state: RootState) => state.auth);
 
   return (
     <SignalrContext.Provider value={connection}>
       <div className="App">
-        <ListNotification/>
-        <Router/>
-        {(isLoading.loading && !isLoading.lockLoading) &&  <Loading/>}
+        <ListNotification />
+        <Router />
+        {(isLoading.loading && !isLoading.lockLoading) && <Loading />}
       </div>
     </SignalrContext.Provider>
-    
+
   )
 }
 
